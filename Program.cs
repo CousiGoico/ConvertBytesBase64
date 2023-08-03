ede6aee28dfbc15d0ed8ab431446bf30559af377
John Doe <johndoe@example.com>
2023-08-03 11:58:48 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program{
+
+    static void Main(string[] args) {
+
+        Console.WriteLine("Introduce el PAT: ");
+        string personalaccesstoken = Console.ReadLine();
+        Console.WriteLine(string.Format("PAT: {0}", personalaccesstoken));
+        string result = Convert.ToBase64String(
+                    System.Text.ASCIIEncoding.ASCII.GetBytes(
+                        string.Format("{0}:{1}", "", personalaccesstoken)));
+        Console.WriteLine(string.Format("Resultado: {0}", result));
+        Console.ReadLine();
+    }
+
+}
+
+
